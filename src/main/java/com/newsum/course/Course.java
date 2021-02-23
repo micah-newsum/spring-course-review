62035cd58df7337e3522dd1d959fb8aada2241ac
Micah Newsum <mnewsum@glenair.com>
2021-02-23 12:03:24 -0800

    Add Course class.

---
+++ src/main/java/com/newsum/course/Course.java
@@ -0,0 +1,42 @@
+package com.newsum.course;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private final Long id;
+  private String title;
+  private String url;
+
+  // zero parameter constructor required by JPA.
+  protected Course(){
+    id = null;
+  }
+
+  public Course(String title, String url) {
+    this();
+    this.title = title;
+    this.url = url;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+}
