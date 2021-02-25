0a795abf591a984d932448aa03f956d126a53309
Micah Newsum <mnewsum@glenair.com>
2021-02-24 21:51:58 -0800

    Add test users.

--- src/main/java/com/newsum/core/DatabaseLoader.java
+++ src/main/java/com/newsum/core/DatabaseLoader.java
@@ -3,20 +3,25 @@ package com.newsum.core;
 import com.newsum.course.Course;
 import com.newsum.course.CourseRepository;
 import com.newsum.review.Review;
+import com.newsum.user.User;
+import com.newsum.user.UserRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
   private final CourseRepository courses;
+  private final UserRepository userDao;
 
-  public DatabaseLoader(CourseRepository courses){
+  public DatabaseLoader(CourseRepository courses, UserRepository userDao){
     this.courses = courses;
+    this.userDao = userDao;
   }
 
   @Override
@@ -41,6 +46,25 @@ public class DatabaseLoader implements ApplicationRunner {
         "Spring HATEOS"
     };
 
+    List<User> students = Arrays.asList(
+        new User("jacobproffer", "Jacob",  "Proffer", "password", new String[] {"ROLE_USER"}),
+        new User("mlnorman", "Mike",  "Norman", "password", new String[] {"ROLE_USER"}),
+        new User("k_freemansmith", "Karen",  "Freeman-Smith", "password", new String[] {"ROLE_USER"}),
+        new User("seth_lk", "Seth",  "Kroger", "password", new String[] {"ROLE_USER"}),
+        new User("mrstreetgrid", "Java",  "Vince", "password", new String[] {"ROLE_USER"}),
+        new User("anthonymikhail", "Tony",  "Mikhail", "password", new String[] {"ROLE_USER"}),
+        new User("boog690", "AJ",  "Teacher", "password", new String[] {"ROLE_USER"}),
+        new User("faelor", "Erik",  "Faelor Shafer", "password", new String[] {"ROLE_USER"}),
+        new User("christophernowack", "Christopher",  "Nowack", "password", new String[] {"ROLE_USER"}),
+        new User("calebkleveter", "Caleb",  "Kleveter", "password", new String[] {"ROLE_USER"}),
+        new User("richdonellan", "Rich",  "Donnellan", "password", new String[] {"ROLE_USER"}),
+        new User("micah", "Newsum",  "mnewsum", "password", new String[] {"ROLE_USER","ROLE_ADMIN"}),
+        new User("albertqerimi", "Albert",  "Qerimi", "password", new String[] {"ROLE_USER"})
+    );
+
+
+    userDao.saveAll(students);
+
     List<Course> mockCourses = new ArrayList<>();
 
     IntStream.range(0,100)
@@ -50,7 +74,9 @@ public class DatabaseLoader implements ApplicationRunner {
                     String buzzword = buzzwords[i % buzzwords.length];
                     String title = String.format(template, buzzword);
                     Course mockCourse = new Course(title, "http://example.com");
-                    mockCourse.addReview(new Review(5,"Great course!"));
+                    Review review = new Review(5, "Great course!");
+                    review.setReviewer(students.get(i % students.size()));
+                    mockCourse.addReview(review);
                     mockCourses.add(mockCourse);
                   }
               );
